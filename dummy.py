05e1f684147f0e0517be609a9e550f18758e37d1
DhanushkiMapitigama <dhanumapitigama@gmail.com>
2025-04-25 13:51:33 +0200

    add is prime function

--- dummy.py
+++ dummy.py
@@ -1,2 +1,19 @@
 # This is a dummy program
 
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+def main():
+    print("Prime numbers between 1 and 1000:")
+    for num in range(1, 1001):
+        if is_prime(num):
+            print(num)
+
+if __name__ == "__main__":
+    main()
+
